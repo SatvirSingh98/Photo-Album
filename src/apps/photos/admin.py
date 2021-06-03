b20147d2eb852a59f1bf1823aec0a8a4e6d75bb7
satvir <satvirsingh.juc@gmail.com>
2021-06-03 23:49:50 +0530

    Filter Photos By Category

--- src/apps/photos/admin.py
+++ src/apps/photos/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Category, Photo
 
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'date')
+
+
 admin.site.register(Category)
-admin.site.register(Photo)
--- src/apps/photos/views.py
+++ src/apps/photos/views.py
@@ -4,8 +4,13 @@ from .models import Category, Photo
 
 
 def gallery(request):
+    category = request.GET.get('category')
+    if category is None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name__contains=category)
+
     categories = Category.objects.all()
-    photos = Photo.objects.all()
     context = {'categories': categories, 'photos': photos}
     return render(request, 'photos/gallery.html', context)
 
