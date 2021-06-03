5b8e066276a6ce2dc9cb9a4234b2e0e79e1c6337
satvir <satvirsingh.juc@gmail.com>
2021-06-03 19:30:06 +0530

    uploading images

--- src/apps/photos/views.py
+++ src/apps/photos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .models import Category, Photo
 
@@ -16,4 +16,21 @@ def view_photo(request, pk=None):
 
 
 def upload_photo(request):
-    return render(request, 'photos/upload_photo.html')
+    categories = Category.objects.all()
+    if request.method == 'POST':
+        category = None
+        images = request.FILES.getlist('images')
+        category_id = request.POST.get('category')
+        new_category = request.POST.get('new_category')
+        if category_id != 'none':
+            category = Category.objects.get(id=category_id)
+        elif new_category != '':
+            category, _ = Category.objects.get_or_create(name=new_category.title())
+
+        for image in images:
+            Photo.objects.create(category=category, image=image)
+
+        return redirect('/')
+
+    context = {'categories': categories}
+    return render(request, 'photos/upload_photo.html', context)
